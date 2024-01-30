39c90daf1081a107e283a781fda7db51c83de1a2
小书童 <2213833089@qq.com>
2024-01-30 09:55:19 +0800

    fix签到

--- src/api/juejin.js
+++ src/api/juejin.js
@@ -1,5 +1,5 @@
-const request = require('../utils/request');
-const { COOKIE } =  require('../utils/config.js');
+const request = require('../utils/request')
+const { COOKIE, AID, MS_TOKEN, LOGID } = require('../utils/config.js')
 
 module.exports = function () {
   return {
@@ -8,34 +8,39 @@ module.exports = function () {
      */
     checkIn: function () {
       return request({
-        url: 'https://api.juejin.cn/growth_api/v1/check_in',
+        url: `https://api.juejin.cn/growth_api/v1/check_in?aid=${AID}&spider=0&msToken=${MS_TOKEN}&a_bogus=EJUmfOZMMsm1qX3FqhDz9JfEQ960YW-ogZEPq5XjwzoS`,
         method: 'post',
         headers: {
           cookie: COOKIE,
-        },
-      });
+          'X-Ms-Token': MS_TOKEN,
+          'X-Tt-Logid': LOGID
+        }
+      })
     },
     /**
      * 抽奖
      */
     drawApi: function () {
       return request({
-        url: 'https://api.juejin.cn/growth_api/v1/lottery/draw',
+        url: `https://api.juejin.cn/growth_api/v1/lottery/draw?aid=${AID}&spider=0&msToken=${MS_TOKEN}`,
         method: 'post',
         headers: {
-          cookie: COOKIE,
-        },
-      });
+          cookie: COOKIE
+        }
+      })
     },
     dipLucky: function (params) {
       return request({
-        url: 'https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky?aid=2608',
+        url:
+          'https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky?aid=2608',
         method: 'post',
         data: params,
         headers: {
           cookie: COOKIE,
-        },
-      });
-    },
+          'X-Ms-Token': MS_TOKEN,
+          'X-Tt-Logid': LOGID
+        }
+      })
+    }
   }
-};
+}
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 module.exports = {
   COOKIE: process.env.COOKIE || '',
+  USERID: process.env.USERID || '',
+  AID: process.env.AID || '',
+  UUID: process.env.UUID || '',
+  MS_TOKEN: process.env.MS_TOKEN || '',
+  LOGID: process.env.LOGID || '',
   TOKEN: process.env.TOKEN || '',
   SERVERID: process.env.SERVERID || '' // server酱
 }
